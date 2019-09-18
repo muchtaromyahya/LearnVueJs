d8e4da405f0caf69d002db0f1a6ee686cfcb7c83
muchtaromyahya <muchtaromyahya@gmail.com>
2019-09-18 14:31:20 +0700

    package

---
+++ cv-muhyhya/src/views/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-app>
+        <v-toolbar app dark>
+            <v-toobar-title class="headline text-uppercase">
+                <span>Vuetify</span>
+                <span class="font-weight-light"> MATERIAL DESIGN </span>
+            </v-toobar-title>>
+            <v-spacer> </v-spacer>
+            <v-btn
+                flat
+                href="https://github.com/vuetifyjs/vuetify/releases/latest"
+                target ="_blank"
+            >
+                <span class="mr-2">Latest Release</span>
+            </v-btn>
+        </v-toolbar>
+        <v-content>
+            <helloworld/>
+        </v-content>
+    </v-app>
+</template>
